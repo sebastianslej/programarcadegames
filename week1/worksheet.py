2629e56119b9a847b54ce2ba6b08ac18ecff1c51
Sebastian Slej <sebastianslej@sebastians-air.lan>
2019-12-04 14:31:19 +0100

    hello

--- week1/worksheet.py
+++ week1/worksheet.py
@@ -1,3 +1,135 @@
-# Question 1: write a line of code that will print your name.
-print("Sebastian")
-# Question 2: How do you enter a comment in a program?
+# 1. Write a line of code that will print your name.
+import a as a
+import push as push
+
+print("sebastian Slej")
+# 2. How do you enter a comment in a program?
+# Like this
+
+# 3. What do the following lines of code output? ALSO: Why do they give a different answer?
+# print(2 / 3)
+# print(2 // 3)
+print(2 / 3)
+print(2 // 3)
+# The latter example is a floor division which will always round the answer down to the nearest integer.
+
+# 4. Write a line of code that creates a variable called pi and sets it to an appropriate value.
+x = 3.14
+print(x)
+
+# 5. Why does this code not work?
+# A = 22
+# print(a)
+# Because of difference in upper and lower case letter
+
+# 6. All of the variable names below can be used. But which ONE of these is the better variable name to use?
+# a
+# A
+# Area
+# AREA
+# area
+# area_of_rectangle
+# Area_Of_Rectangle
+# Answer = area_of_rectangle
+
+# 7 Which of these variables names are not allowed in Python? (More than one might be wrong. Also, this question is not asking about improper names, just names that aren't allowed. Test them if you aren't sure.)
+# 1Apple
+# account number
+# account.number
+# great.big.variable
+# 2x
+# #left
+
+# 8. Why does this code not work?
+# print(a)
+# a = 45
+# Answer = The variable hasn't been determined prior to printing
+
+# 9. Explain the mistake in this code:
+# pi = float(3.14)
+# Answer = On the right side of the assignment operator must be and expression - in this case pi = 3.14
+
+# 10. This program runs, but the code still could be better. Explain what is wrong with the code.
+# radius = float(input("Radius:"))
+# x = 3.14
+# pi = x
+# area = pi  * radius ** 2
+# print(area)
+# Answer = The input variables for the radius hasn't been defined??
+
+# 11. Explain the mistake in the following code:
+# x = 4
+# y = 5
+# a = ((x) * (y))
+# print(a)
+
+# Answer= There are too many brackets parenthesis !
+
+# 12. Explain the mistake in the following code:
+# x = 4
+# y = 5
+# a = 3(x + y)
+# print(a)
+
+# Answer = its missing a multiplication operator
+
+
+# 13. Explain the mistake in the following code:
+# radius = input(float("Enter the radius:"))
+
+# Answer = The input variable hasn't been defined ?
+
+# 14 Do all these print the same value? Which one is better to use and why?
+# print(2/3+4)
+# print(2 / 3 + 4)
+# print(   2 /    3+    4  )
+
+# answer = number 2 is the best style !
+
+# 15. What is a constant?
+# Answer = A variable that isn't variable
+
+# 16. How are variable names for constants different than other variable names?
+# answer = ALL UPPER CASE LETTERS!
+
+# 17. What is a single quote and what is a double quote? Give and label an example of both.
+# Answer = A single quote is this ' and a double quote is this "
+# Examples: Print ("Hi") or
+# Print("hi")
+# '''
+# this is etc.
+
+# 18.  Write a Python program that will use escape codes to print a double-quote and a new line using the Window's standard. (Note: I'm asking for the Window's standard here. Look it up out of Chapter 1.)
+# Answer: print("this\r\nis\r\ndifficult.")
+
+# 19.  Can a Python program print text to the screen using single quotes instead of double quotes?
+# Asnwer = Yes by doing '''
+
+# 20. Why does this code not calculate the average?
+# print(3 + 4 + 5 / 3)
+# Answer = Because it isnt print(1 + 4 + 5 ) / 5
+
+# 21. What is an ``operator'' in Python?
+# Answer = An operator is a symbiol like + or -.
+
+# 22. What does the following program print out?
+# x = 3
+# x + 1
+# print(x)
+
+# Answer = It prints out 3
+
+
+# 23. Correct the following code:
+# user_name = input("Enter your name: )"
+
+# Answer: ?!?!?
+
+# 24. Correct the following code:
+# value = int(input(print("Enter your age")))
+
+# Answer: !??!
+
+git commit -a -m "Added stuff"
+
+git push
